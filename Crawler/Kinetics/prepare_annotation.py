9c507078087687273c1287c7dfc5e695f69fa1f9
Yiren Jian <yirenjian0621@gmail.com>
2023-12-29 18:17:44 -0500

    [update] prepare annotations

--- Crawler/Kinetics/prepare_annotation.py
+++ Crawler/Kinetics/prepare_annotation.py
@@ -3,6 +3,7 @@ import json
 import os
 from tqdm import tqdm
 
+#### training videos
 with open('vatex_training_v1.0.json') as f:
     d = json.load(f)
 
@@ -18,3 +19,37 @@ for vid in tqdm(d):
 
 with open('cap_train.json', 'w') as f:
     json.dump(anns, f)
+
+##### validation videos
+with open('vatex_validation_v1.0.json') as f:
+    d = json.load(f)
+
+anns = []
+for vid in tqdm(d):
+    v_name = vid["videoID"]
+    v_name = "videos/val/" + v_name + ".mp4"
+    if os.path.isfile(v_name):
+        for i in range(len(vid["enCap"])):
+            anns.append({"image_id": v_name, "video": v_name, "caption": vid["enCap"][i]})
+    else:
+        pass
+
+with open('vatex_val_gt.json', 'w') as f:
+    json.dump({"annotation": anns}, f)
+
+#### test videos
+with open('vatex_public_test_english_v1.1.json') as f:
+    d = json.load(f)
+
+anns = []
+for vid in tqdm(d):
+    v_name = vid["videoID"]
+    v_name = "videos/test/" + v_name + ".mp4"
+    if os.path.isfile(v_name):
+        for i in range(len(vid["enCap"])):
+            anns.append({"image_id": v_name, "video": v_name, "caption": vid["enCap"][i]})
+    else:
+        pass
+
+with open('vatex_test_gt.json', 'w') as f:
+    json.dump({"annotation": anns}, f)
